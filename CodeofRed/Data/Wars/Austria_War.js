812ebf1cccf929996f495f59f2a6f00dcca83a56
Ambika <ambika@Ambikas-MacBook-Pro.local>
2017-09-01 17:01:42 -0700

    Removing

--- CodeofRed/Data/Wars/Austria_War.js
+++
@@ -1,7 +0,0 @@
-var Austria_War= [
-	{"end_year": "1901", "start_year": "1900", "war_name": "Boxer Rebellion"},
-	{"end_year": "1918", "start_year": "1914", "war_name": "World War I"},
-	{"end_year": "1921", "start_year": "1921", "war_name": "Uprising in West Hungary"},
-	{"end_year": "1945", "start_year": "1935", "war_name": "World War II"},
-	{"end_year": "2016", "start_year": "2001", "war_name": "War in Afghanistan"}
-]
--- CodeofRed/Data/Wars/Switzerland_War.js
+++
@@ -1,2 +0,0 @@
-var Switzerland_War= [
-]
