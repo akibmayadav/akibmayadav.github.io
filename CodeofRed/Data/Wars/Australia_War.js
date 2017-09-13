37a9a9e4e51bb3afcb86afac1d0956ec4a44cbf7
Ambika <ambika@Ambikas-MacBook-Pro.local>
2017-09-12 20:06:49 -0400

    Code Of Red

---
+++ CodeofRed/Data/Wars/Australia_War.js
@@ -0,0 +1,15 @@
+var Australia_War= [
+	{"end_year": "1902", "start_year": "1900", "war_name": "Second Boer War"},
+	{"end_year": "1918", "start_year": "1914", "war_name": "World War I"},
+	{"end_year": "1919", "start_year": "1918", "war_name": "Allied Intervention in the Russian Civil War"},
+	{"end_year": "1945", "start_year": "1939", "war_name": "World War II"},
+	{"end_year": "1960", "start_year": "1948", "war_name": "Malyan Emergency"},
+	{"end_year": "1953", "start_year": "1950", "war_name": "Korean War"},
+	{"end_year": "1966", "start_year": "1963", "war_name": "Borneo Confrontation"},
+	{"end_year": "1973", "start_year": "1964", "war_name": "Vietnam War"},
+	{"end_year": "1991", "start_year": "1990", "war_name": "Gulf War"},
+	{"end_year": "1995", "start_year": "1992", "war_name": "Operation Restore Hope"},
+	{"end_year": "2016", "start_year": "2001", "war_name": "War in Afghanistan"},
+	{"end_year": "2011", "start_year": "2003", "war_name": "Iraq War"},
+	{"end_year": "2016", "start_year": "2014", "war_name": "War on ISIL"}
+]
---
+++ CodeofRed/Data/Wars/Switzerland_War.js
@@ -0,0 +1,2 @@
+var Switzerland_War= [
+]
